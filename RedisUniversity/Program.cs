f1dd2adf90d1b6d4fe3cf21a1ba64e1392be8f20
GençerGaming <GenerGaming@DESKTOP-QLQOFTI>
2023-02-06 23:55:01 +0300

    Add project files.

---
+++ RedisUniversity/Program.cs
@@ -0,0 +1,22 @@
+﻿using StackExchange.Redis;
+
+namespace RedisUniversity
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Redis!");
+
+            var muxer = ConnectionMultiplexer.Connect(new ConfigurationOptions
+            {
+                EndPoints = new EndPointCollection { "localhost:6379" }
+            });
+
+            var db = muxer.GetDatabase();
+            var res = db.Ping();
+            Console.WriteLine($"The ping took: {res.TotalMilliseconds} ms");
+            //End Programming Challenge 
+        }
+    }
+}
